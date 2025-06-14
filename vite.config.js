6d46c627768c21d6618b59752054d6e2bb705234
Ada <adawinlee@gmail.com>
2025-06-14 00:32:27 -0700

    github pages test 7

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://adawinlee.github.io/601proj/",
+  base: "https://adawinlee.github.io/",
   server: {
     host: true,
   },
