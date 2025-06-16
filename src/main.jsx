5aa9314c989ed197a71d2e129dc5acfb0da2dbc0
Ada <adawinlee@gmail.com>
2025-06-16 15:13:36 -0700

    fixing routing test 4

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='https://adawinlee.github.io/601proj/'>
       <App />
     </BrowserRouter>
   </StrictMode>,
